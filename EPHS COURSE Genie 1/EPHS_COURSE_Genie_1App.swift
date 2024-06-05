a1d6aa726ec57d7d95a0d8e7eefa16f9f4f6e3c7
Rishi Naagappan <rishi.naagappan.2007@gmail.com>
2024-06-05 10:25:09 -0500

    Initial Commit

---
+++ EPHS COURSE Genie 1/EPHS_COURSE_Genie_1App.swift
@@ -0,0 +1,17 @@
+//
+//  EPHS_COURSE_Genie_1App.swift
+//  EPHS COURSE Genie 1
+//
+//  Created by 64001200 on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EPHS_COURSE_Genie_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
